a472a3fcbffa9df55862bf3ce74e12fe4d456826
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2016-05-08 00:26:53 +0200

    First commit

---
+++ manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from os import environ
+from sys import argv
+
+if __name__ == "__main__":
+    environ.setdefault("DJANGO_SETTINGS_MODULE", "jdb.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(argv)
