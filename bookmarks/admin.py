dfd521323f654050a01fadb92b67d074cdcd4c77
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2016-05-09 21:36:37 +0200

    Add some marks to mark

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from bookmarks.models import (
+    Bookmark, BookmarkForm, Book, BookForm, ReadLater, ReadLaterForm,
+    News, NewsForm, People, PeopleForm)
 
-# Register your models here.
+
+class BookmarkAdmin(admin.ModelAdmin):
+    form = BookmarkForm
+    list_display = ('category', 'name', 'note',)
+    search_fields = ('category__name', 'name', 'note',)
+
+
+class BookAdmin(admin.ModelAdmin):
+    form = BookForm
+    list_display = ('category', 'title', 'author', 'note', 'read',)
+    search_fields = ('category__name', 'title', 'author', 'note',)
+
+
+class ReadLaterAdmin(admin.ModelAdmin):
+    form = ReadLaterForm
+    list_display = ('category', 'name', 'note',)
+    search_fields = ('category__name', 'name', 'note',)
+
+
+class NewsAdmin(admin.ModelAdmin):
+    form = NewsForm
+    list_display = ('category', 'name', 'rss', 'note',)
+    search_fields = ('category__name', 'name', 'note',)
+
+
+class PeopleAdmin(admin.ModelAdmin):
+    form = PeopleForm
+    list_display = ('category', 'name', 'note',)
+    search_fields = ('category__name', 'name', 'note',)
+
+admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(ReadLater, ReadLaterAdmin)
+admin.site.register(News, NewsAdmin)
+admin.site.register(People, PeopleAdmin)
--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,88 @@
-from django.db import models
+from django.db.models import Model, CharField, ForeignKey, BooleanField
+from django.forms import ModelForm, CharField as formCharField, Textarea
 
-# Create your models here.
+from common.models import Category
+
+
+class Bookmark(Model):
+    category = ForeignKey(Category)
+    name = CharField(max_length=256, primary_key=True)
+    note = CharField(max_length=256)
+
+
+class BookmarkForm(ModelForm):
+    note = formCharField(required=False, widget=Textarea)
+
+    class Meta:
+        model = Bookmark
+        fields = ('category', 'name', 'note',)
+
+
+class Book(Model):
+    category = ForeignKey(Category)
+    title = CharField(max_length=64, primary_key=True)
+    author = CharField(max_length=64)
+    note = CharField(max_length=256)
+    read = BooleanField(default=False)
+
+
+class BookForm(ModelForm):
+    note = formCharField(required=False, widget=Textarea)
+
+    class Meta:
+        model = Book
+        fields = ('category', 'title', 'author', 'note', 'read',)
+
+
+class ReadLater(Model):
+    category = ForeignKey(Category)
+    name = CharField(max_length=256)
+    note = CharField(max_length=256)
+
+    class Meta:
+        verbose_name_plural = 'reads later'
+
+
+class ReadLaterForm(ModelForm):
+    note = formCharField(required=False, widget=Textarea)
+
+    class Meta:
+        model = ReadLater
+        fields = ('category', 'name', 'note',)
+
+
+class News(Model):
+    category = ForeignKey(Category)
+    name = CharField(max_length=256)
+    rss = CharField(max_length=256, null=True)
+    note = CharField(max_length=256)
+
+    class Meta:
+        verbose_name_plural = 'news'
+
+
+class NewsForm(ModelForm):
+    note = formCharField(required=False, widget=Textarea)
+    rss = formCharField(required=False)
+
+    class Meta:
+        model = News
+        fields = ('category', 'name', 'rss', 'note',)
+
+
+class People(Model):
+    """Interesting people *.*"""
+    category = ForeignKey(Category)
+    name = CharField(max_length=256, primary_key=True)
+    note = CharField(max_length=256)
+
+    class Meta:
+        verbose_name_plural = 'people'
+
+
+class PeopleForm(ModelForm):
+    note = formCharField(required=False, widget=Textarea)
+
+    class Meta:
+        model = People
+        fields = ('category', 'name', 'note',)
