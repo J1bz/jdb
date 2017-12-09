dcf35c087c107d57e495473eb31f594347ba0d79
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2017-12-10 00:11:14 +0100

    Add Movie model

--- media/admin.py
+++ media/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from media.models import MediaCategory, MediaCategoryForm, Movie, MovieForm
+
+
+class MediaCategoryAdmin(admin.ModelAdmin):
+    form = MediaCategoryForm
+    list_display = ('name', 'note',)
+    search_fields = ('name', 'note',)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    form = MovieForm
+    list_display = (
+        'title',
+        'get_category_list',
+        'realisator',
+        'rating',
+        'to_watch_again',
+        'seen',
+    )
+    search_fields = ('title', 'categories__name', 'realisator', 'note',)
+
+    def get_category_list(self, obj):
+        return ', '.join([category.name for category in obj.categories.all()])
+
+    get_category_list.short_description = 'categories'
+
+
+admin.site.register(MediaCategory, MediaCategoryAdmin)
+admin.site.register(Movie, MovieAdmin)
--- media/models.py
+++ media/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django import forms
 
-# Create your models here.
+
+class MediaCategory(models.Model):
+    name = models.CharField(max_length=32, primary_key=True)
+    note = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name_plural = 'media categories'
+
+    def __str__(self):
+        return self.name
+
+
+class MediaCategoryForm(forms.ModelForm):
+    note = forms.CharField(required=False, widget=forms.Textarea)
+
+    class Meta:
+        model = MediaCategory
+        fields = '__all__'
+
+
+class Movie(models.Model):
+    categories = models.ManyToManyField(MediaCategory)
+    title = models.CharField(max_length=64, primary_key=True)
+    realisator = models.CharField(max_length=64)
+    rating = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0)
+        ]
+    )
+    to_watch_again = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0)
+        ]
+    )
+    note = models.CharField(max_length=1024)
+    seen = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class MovieForm(forms.ModelForm):
+    note = forms.CharField(required=False, widget=forms.Textarea)
+    realisator = forms.CharField(required=False)
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
